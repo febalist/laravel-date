a5385c819f09a865b8806033938eb6febfd543b6
febalist <febalist@gmail.com>
2019-07-19 00:06:46 +0300

    Extends Febalist\Calendar and call Jenssegers\Date

--- src/Date.php
+++ src/Date.php
@@ -2,20 +2,34 @@
 
 namespace Febalist\Laravel\Date;
 
-use BadMethodCallException;
 use Febalist\Calendar\Calendar;
 use Jenssegers\Date\Date as JenssegersDate;
 
-/** @mixin Calendar */
-class Date extends JenssegersDate
+/** @mixin JenssegersDate */
+class Date extends Calendar
 {
-    public function __call($method, $parameters)
+    public function ago($since = null, $syntax = null, $short = false, $parts = 1, $options = null)
     {
-        try {
-            return parent::__call($method, $parameters);
-        } catch (BadMethodCallException $exception) {
-            $calendar = new Calendar($this);
-            return $calendar->$method(...$parameters);
-        }
+        return $this->jenssegers('ago', [$since, $syntax, $short, $parts, $options]);
+    }
+
+    public function until($since = null, $syntax = null, $short = false, $parts = 1, $options = null)
+    {
+        return $this->jenssegers('until', [$since, $syntax, $short, $parts, $options]);
+    }
+
+    public function format($format)
+    {
+        return $this->jenssegers('format', [$format]);
+    }
+
+    public function timespan($time = null, $timezone = null)
+    {
+        return $this->jenssegers('timespan', [$time, $timezone]);
+    }
+
+    protected function jenssegers($method, $parameters)
+    {
+        return JenssegersDate::parse($this)->$method(...$parameters);
     }
 }
