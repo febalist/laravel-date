7e1656bb0f82a68a9a15a8bc53186757dcf662a7
febalist <febalist@gmail.com>
2019-07-18 22:51:52 +0300

    Magic Calendar

---
+++ _ide_helper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Illuminate\Support {
+
+    use Febalist\Laravel\Date\Date;
+
+    /** @mixin Date */
+    class Carbon
+    {
+
+    }
+}
--- src/Date.php
+++ src/Date.php
@@ -2,9 +2,20 @@
 
 namespace Febalist\Laravel\Date;
 
+use BadMethodCallException;
+use Febalist\Calendar\Calendar;
 use Jenssegers\Date\Date as JenssegersDate;
 
+/** @mixin Calendar */
 class Date extends JenssegersDate
 {
-
+    public function __call($method, $parameters)
+    {
+        try {
+            return parent::__call($method, $parameters);
+        } catch (BadMethodCallException $exception) {
+            $calendar = new Calendar($this);
+            return $calendar->$method(...$parameters);
+        }
+    }
 }
