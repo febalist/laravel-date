85c557513a7bd1e60cf34706166841c5eeae57ec
febalist <febalist@gmail.com>
2020-03-29 15:31:45 +0300

    Fix Maximum function nesting level reached

--- src/Date.php
+++ src/Date.php
@@ -10,26 +10,27 @@ class Date extends Calendar
 {
     public function ago($since = null, $syntax = null, $short = false, $parts = 1, $options = null)
     {
-        return $this->jenssegers('ago', [$since, $syntax, $short, $parts, $options]);
+        return $this->jenssegers()->ago($since, $syntax, $short, $parts, $options);
     }
 
     public function until($since = null, $syntax = null, $short = false, $parts = 1, $options = null)
     {
-        return $this->jenssegers('until', [$since, $syntax, $short, $parts, $options]);
+        return $this->jenssegers()->until($since, $syntax, $short, $parts, $options);
     }
 
     public function format($format)
     {
-        return $this->jenssegers('format', [$format]);
+        return $this->jenssegers()->format($format);
     }
 
     public function timespan($time = null, $timezone = null)
     {
-        return $this->jenssegers('timespan', [$time, $timezone]);
+        return $this->jenssegers()->timespan($time, $timezone);
     }
 
-    protected function jenssegers($method, $parameters)
+    /** @return JenssegersDate */
+    protected function jenssegers()
     {
-        return JenssegersDate::parse($this)->$method(...$parameters);
+        return new JenssegersDate($this->toDateTimeString(), $this->getTimezone());
     }
 }
